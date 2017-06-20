9dfaa553181b7ad8edee25f86a3164bbeaacee2c
patyalves17 <patriciaalves10@gmail.com>
2017-06-19 23:06:25 -0300

    listacarros

--- app/src/main/java/com/example/paty/carros/APIUtils.java
+++ app/src/main/java/com/example/paty/carros/APIUtils.java
@@ -12,6 +12,6 @@ public class APIUtils {
     public static CarroAPI getAPIService() {
         return RetrofitClient.getClient(BASE_URL).create(CarroAPI.class);
     }
- 
+
 
 }
--- app/src/main/java/com/example/paty/carros/MainActivity.java
+++ app/src/main/java/com/example/paty/carros/MainActivity.java
@@ -2,6 +2,13 @@ package com.example.paty.carros;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +16,21 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        CarroAPI carroAPI=APIUtils.getAPIService();
+        carroAPI.listarTodos().enqueue(new Callback<List<Carro>>() {
+            @Override
+            public void onResponse(Call<List<Carro>> call, Response<List<Carro>> response) {
+                for(Carro carro:response.body()) {
+                    Log.i("Carro", carro.getMarca());
+                }
+            }
+
+            @Override
+            public void onFailure(Call<List<Carro>> call, Throwable t) {
+
+            }
+        });
     }
 }
+ 

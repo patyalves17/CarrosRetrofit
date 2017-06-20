c8342100d07501adbf934b569cf4a7b154763ca1
patyalves17 <patriciaalves10@gmail.com>
2017-06-19 22:53:12 -0300

    APIUtils

--- app/src/main/java/com/example/paty/carros/APIUtils.java
+++ app/src/main/java/com/example/paty/carros/APIUtils.java
@@ -5,5 +5,13 @@ package com.example.paty.carros;
  */
 
 public class APIUtils {
+    private APIUtils() {}
+
+    public static final String BASE_URL = "http://10.3.1.10:3000";
+
+    public static CarroAPI getAPIService() {
+        return RetrofitClient.getClient(BASE_URL).create(CarroAPI.class);
+    }
+
 
 }

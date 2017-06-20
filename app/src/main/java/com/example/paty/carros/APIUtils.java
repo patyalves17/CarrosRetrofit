0db14424fe6d2248407b80c31b63f9957e9cddbf
patyalves17 <patriciaalves10@gmail.com>
2017-06-19 22:50:48 -0300

    CarroAPI

---
+++ app/src/main/java/com/example/paty/carros/APIUtils.java
@@ -0,0 +1,9 @@
+package com.example.paty.carros;
+
+/**
+ * Created by logonrm on 19/06/2017.
+ */
+
+public class APIUtils {
+
+}
---
+++ app/src/main/java/com/example/paty/carros/CarroAPI.java
@@ -0,0 +1,21 @@
+package com.example.paty.carros;
+
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.http.Body;
+import retrofit2.http.GET;
+import retrofit2.http.POST;
+
+/**
+ * Created by logonrm on 19/06/2017.
+ */
+
+public interface CarroAPI {
+    @POST("/carros")
+    Call<Void> salvar(@Body Carro carro);
+
+    @GET("/carros")
+    Call<List<Carro>> listarTodos();
+
+}
